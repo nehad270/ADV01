5845d6dcf02699cd6cba6e64e77492623280a802
nehad <Dark@DESKTOP-TQDVAO4>
2025-08-29 01:23:52 -0700

    qu1

--- Program.cs
+++ Program.cs
@@ -1,24 +1,32 @@
-﻿namespace ADV01
+﻿using System.Drawing;
+
+namespace ADV01
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Point p1=new Point(10, 3);
+            Point p2=new Point(4, 2);
+
+            Swap.swap<Point>(ref p1, ref p2);
+            Console.WriteLine(p1);
+
             int[] numbers = { 5, 3, 8, 4, 2 };
-            Sorter.OptimizedBubbleSort(numbers);
+            Swap.buble(numbers);
 
             Console.WriteLine("Sorted array:");
             foreach (var num in numbers)
                 Console.Write(num + " ");
 
 
-            var intRange = new Range<int>(10, 20);
-            Console.WriteLine($"Is 15 in range? {intRange.IsInRange(15)}");
-            Console.WriteLine($"Length: {intRange.Length()}");
+            //var intRange = new Range<int>(10, 20);
+            //Console.WriteLine($"Is 15 in range? {intRange.IsInRange(15)}");
+            //Console.WriteLine($"Length: {intRange.Length()}");
 
-            var doubleRange = new Range<double>(5.5, 10.2);
-            Console.WriteLine($"Is 7.1 in range? {doubleRange.IsInRange(7.1)}");
-            Console.WriteLine($"Length: {doubleRange.Length()}");
+            //var doubleRange = new Range<double>(5.5, 10.2);
+            //Console.WriteLine($"Is 7.1 in range? {doubleRange.IsInRange(7.1)}");
+            //Console.WriteLine($"Length: {doubleRange.Length()}");
         }
     }
 }
---
+++ Swap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADV01
+{
+    internal class Swap
+    {
+        public static void swap<T>(ref T a,ref T b) 
+        {
+            T temp = a;
+            a = b;
+
+            b = temp;
+        }
+        public static void buble(int[] array) 
+        {
+            if (array is not null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for(int j = 0;j<array.Length-1-i;j++)
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            swap(ref array[j], ref array[j + 1]);
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
